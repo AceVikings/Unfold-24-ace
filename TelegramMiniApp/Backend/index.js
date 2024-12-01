441dc3483d7f9c5e9ad1571852b93475d66695de
Ace <51465755+AceVikings@users.noreply.github.com>
2024-12-01 16:54:22 +0530

    update backend

--- TelegramMiniApp/Backend/index.js
+++ TelegramMiniApp/Backend/index.js
@@ -1,10 +1,59 @@
 const express = require("express");
-
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_API_KEY}`;
+const WEB_URL = "https://unfold-24-ace.onrender.com";
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/bot", async (req, res) => {
+  console.log("Request Body", JSON.stringify(req.body));
+  const message = req.body.message;
+
+  if (message && message.text === "/start") {
+    const response = `Welcome to Ez Portfolio Manager Mini App. You can use this automatically manage your portfolio.`;
+
+    try {
+      const inlineKeyboardMarkup = {
+        inline_keyboard: [
+          [
+            {
+              text: "Start Now!",
+              web_url: { url: `${WEB_URL}?startapp=fullscreen` },
+            },
+          ],
+        ],
+      };
+
+      const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: message.chat.id,
+          text: response,
+          parse_mode: "Markdown",
+          reply_markup: JSON.stringify(inlineKeyboardMarkup),
+        }),
+      });
+    } catch (error) {
+      console.error("Error", error);
+    }
+  } else {
+    console.log("Invalid Request");
+    res.status(200).send("No action taken");
+  }
 });
 
 app.listen(PORT, () => {
